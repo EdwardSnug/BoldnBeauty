2f49ea238921b038336ac29d9f3a3a6d4c0f57e7
Edward Karogo <prezokams@gmail.com>
2025-06-28 12:24:21 +0300

    Updating Boldnbeauty project

--- src/auth.js
+++ src/auth.js
@@ -1,80 +1,102 @@
-// // Button click event listeners
-const signupbtn = document.querySelector("#Signup");
-const formSignup = document.querySelector("#signup-form")
-const loginbtn = document.querySelector("#login");
-const formLogin = document.querySelector("#login-form");
-const noSignupbtn = document.querySelector("#noSignup");
-//SignUp and Login buttons display forms
-signupbtn.addEventListener("click", () => {
-    //Display the form input fields for signing up
-    formSignup.style.display = "flex";
-    formSignup.style.flexDirection = "column";
-    //Hide buttons
-    signupbtn.style.display = "none"
-    loginbtn.style.display = "none"
-    noSignupbtn.style.display = "none";
-})
-loginbtn.addEventListener("click", () => {
-    //Display the form input fields for signing up
-    formLogin.style.display = "flex";
-    formLogin.style.flexDirection = "column";
-    //Hide other buttons
-    loginbtn.style.display = "none"
-    signupbtn.style.display = "none"
-    noSignupbtn.style.display = "none";
-})
-const cancelbtn1 = document.querySelector("#cancel1")
-const cancelbtn2 = document.querySelector("#cancel2")
-cancelbtn1.addEventListener("click", () => {
-    //On clicking the cancel button the forms to be hidden
-    formSignup.style.display = "none"
-    noSignupbtn.style.display = "block";
-})
-cancelbtn2.addEventListener("click", () => {
-    //On clicking the cancel button the forms to be hidden
-    formLogin.style.display = "none"
-    noSignupbtn.style.display = "block";
-})
-//Signup logic
-formSignup.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const username = document.getElementById("signup-username").value.trim();
-    const password = document.getElementById("signup-password").value;
-    //Using localStorage to keep the session record
-    let users = JSON.parse(localStorage.getItem("users") || "[]");
+//DOMContentLoaded
+document.addEventListener("DOMContentLoaded", () => {
 
-    if (users.some(user => user.username === username)) {
-        alert("Username already exists.");
-    } else {
-        users.push({ username, password });
-        localStorage.setItem("users", JSON.stringify(users));
-        alert("Signup successful! You can now log in.");
-        this.reset();
-        this.style.display = "none";
-    }
-})
-//  Login logic
-formLogin.addEventListener("submit", function (e) {
-    e.preventDefault();
+    // Button click event listeners
+    const signupbtn = document.querySelector("#Signup");
+    const formSignup = document.querySelector("#signup-form")
+    const loginbtn = document.querySelector("#login");
+    const formLogin = document.querySelector("#login-form");
+    const noSignupbtn = document.querySelector("#noSignup");
+    //SignUp and Login buttons display forms
+    signupbtn.addEventListener("click", () => {
+        //Display the form input fields for signing up
+        formSignup.style.display = "flex";
+        formSignup.style.flexDirection = "column";
+        //Hide buttons
+        signupbtn.style.display = "none"
+        loginbtn.style.display = "none"
+        noSignupbtn.style.display = "none";
+    })
+    loginbtn.addEventListener("click", () => {
+        //Display the form input fields for signing up
+        formLogin.style.display = "flex";
+        formLogin.style.flexDirection = "column";
+        //Hide other buttons
+        loginbtn.style.display = "none"
+        signupbtn.style.display = "none"
+        noSignupbtn.style.display = "none";
+    })
+    const cancelbtn1 = document.querySelector("#cancel1")
+    const cancelbtn2 = document.querySelector("#cancel2")
+    cancelbtn1.addEventListener("click", () => {
+        //On clicking the cancel button the forms to be hidden
+        formSignup.style.display = "none"
+        //Display continue without an Account button.
+        noSignupbtn.style.display = "block";
+    })
+    cancelbtn2.addEventListener("click", () => {
+        //On clicking the cancel button the forms to be hidden
+        formLogin.style.display = "none"
+        noSignupbtn.style.display = "block";
+    })
+    //Signup logic
+    formSignup.addEventListener("submit", (e) => {
+        e.preventDefault();
+        //creating variables to store our username and password for each user
+        const username = document.getElementById("signup-username").value.trim();
+        const password = document.getElementById("signup-password").value;
+        //Firstly we will create our users data array
+        //Retrieve user data from the browser's local storage and parses it into our users array
+        //Using localStorage to keep the session record
+        //localStorage.getItem("users") -> this method retrieves a string value from localStorage by its key.
+        //Syntax:localStorage.getItem(key)
+        //uses logical OR to check if record exists 
+        let users = JSON.parse(localStorage.getItem("users") || "[]");
+        const existingUser = users.find(user => user.username === username);
 
-    const username = document.getElementById("login-username").value.trim();
-    const password = document.getElementById("login-password").value;
+        if (existingUser) {
+            alert("Username already exists.");
+        } else {
+            users.push({ username, password });
+            localStorage.setItem("users", JSON.stringify(users));
+            alert("Signup successful! You can now log in.");
+            this.reset();
+            this.style.display = "none";
+        }
 
-    let users = JSON.parse(localStorage.getItem("users") || "[]");
+        // if (users.some(user => user.username === username)) {
+        //     alert("Username already exists.");
+        // } else {
+        //     users.push({ username, password });
+        //     localStorage.setItem("users", JSON.stringify(users));
+        //     alert("Signup successful! You can now log in.");
+        //     this.reset(); 
+        
+    })
+    //  Login logic
+    formLogin.addEventListener("submit", function (e) {
+        e.preventDefault();
 
-    const match = users.find(user => user.username === username && user.password === password);
+        const username = document.getElementById("login-username").value.trim();
+        const password = document.getElementById("login-password").value;
 
-    if (match) {
-        alert(`Welcome, ${username}! Redirecting to Home page...`);
-        localStorage.setItem("loggedInUser", username);
+        let users = JSON.parse(localStorage.getItem("users") || "[]");
 
-        // Redirect to Home Page
+        const match = users.find(user => user.username === username && user.password === password);
+
+        if (match) {
+            alert(`Welcome, ${username}! Redirecting to Home page...`);
+            localStorage.setItem("loggedInUser", username);
+            this.reset();
+
+            // Redirect to Home Page
+            window.location.href = "Homepage.html";
+        } else {
+            alert("Invalid username or password.");
+        }
+    });
+    noSignupbtn.addEventListener("click", (e) => {
+        e.preventDefault();
         window.location.href = "Homepage.html";
-    } else {
-        alert("Invalid username or password.");
-    }
-});
-noSignupbtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    window.location.href = "Homepage.html";
-}) 
+    })
+})
--- src/home.js
+++ src/home.js
@@ -1,7 +1,9 @@
-//This JS file is for implimenting functionality and logic to my homepage.html file
-//Get button for products page
-const shopbtn = document.querySelector("#shopNow");
-shopbtn.addEventListener("click", (e) =>{
-    e.preventDefault();
-     window.location.href = "./products.html";
- })
+document.addEventListener("DOMContentLoaded", () => {
+    //This JS file is for implimenting functionality and logic to my homepage.html file
+    //Get button for products page
+    const shopbtn = document.querySelector("#shopNow");
+    shopbtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        window.location.href = "./products.html";
+    })
+})
