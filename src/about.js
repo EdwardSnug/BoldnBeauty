cb31196b1e993c33b79770e6ca9e477c5bf02dda
Edward Karogo <prezokams@gmail.com>
2025-06-29 23:39:35 +0300

    Updating my About us page

--- src/about.js
+++ src/about.js
@@ -1,26 +1,30 @@
-document.addEventListener("DOMContentLoaded", e => {
-    e.preventDefault();
-    //const apiEndpoint =  "http://localhost:3000/review"
+document.addEventListener("DOMContentLoaded", () => {
+    const apiEndpoint =  "https://flowermaniaapi.onrender.com/reviews"
+    //Select our form using its Id
     const formy = document.querySelector("#myform")
+    //Select our button
     const getinTouchbtn = document.querySelector("#submitbtn");
-    console.log(getinTouchbtn);
-    let review = [];
-    formy.addEventListener("submit", ()=>{
+    formy.addEventListener("submit", (e)=>{
+        e.preventDefault();
         const formDta = new FormData(formy);
+        const review = {};
         formDta.forEach((value,key)=>{
+            //Populate our object with key value pairs
             review[key] = value;
         })
+        //send to server
         pushdta(review);
     })
     const pushdta = (obj) => {
         fetch(apiEndpoint, {
             method: "POST",
-            headers: {"Content-type":"application-json"},
+            headers: {"Content-Type":"application/json"},
             body:JSON.stringify(obj)
         })
         .then(res => res.json())
         .then(data =>{
             alert("Data Sent successfully we wil be in touch")
+            formy.reset();
         })
         .catch(error => console.log(error));
         
